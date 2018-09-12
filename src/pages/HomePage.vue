187ced79feba335028edb9a380fb7f53a7774f1e
liqinglin <1317971527@qq.com>
2018-09-12 22:26:35 +0800

    init repo

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <span>homePage</span>
+    <tabbar v-model="index">
+      <tabbar-item link="home">
+        <img slot="icon" src="../assets/icon/home.svg">
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item link="msg">
+        <img slot="icon" src="../assets/icon/msg.svg">
+        <span slot="label">留言板</span>
+      </tabbar-item>
+      <tabbar-item link="money">
+        <img slot="icon" src="../assets/icon/money.svg">
+        <span slot="label">金融板块</span>
+      </tabbar-item>
+      <tabbar-item link="my">
+        <img slot="icon" src="../assets/icon/person.svg">
+        <span slot="label">个人中心</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+export default {
+  name: 'HomePage',
+  components: {
+    Tabbar,
+    TabbarItem
+  },
+  data () {
+    return {
+        index: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HomePage from '@/pages/HomePage'
+import MsgPage from '@/pages/MsgPage'
+import MoneyPage from '@/pages/MoneyPage'
+import MyPage from '@/pages/MyPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'HomePage',
+      component: HomePage
+    },
+    {
+      path: '/msg',
+      name: 'MsgPage',
+      component: MsgPage
+    },
+    {
+      path: '/money',
+      name: 'MoneyPage',
+      component: MoneyPage
+    },
+    {
+      path: '/my',
+      name: 'MyPage',
+      component: MyPage
+    }
+  ]
+})
